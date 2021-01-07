b64ee296b2d391d214639ea67c0db6a9a564caf8
Adrien RICHARD <richardadrien0@gmail.com>
2021-01-07 20:12:28 +0100

    added more remote

--- software/RGB LED controller/include/remoteCodes.h
+++ software/RGB LED controller/include/remoteCodes.h
@@ -11,34 +11,34 @@
 #define IR44_REDDISH       0xFF2AD5  // 
 #define IR44_GREENISH      0xFFAA55  // 
 #define IR44_DEEPBLUE      0xFF926D  // 
-#define IR44_WARMWHITE2    0xFF12ED  // warmest white
-#define IR44_ORANGE        0xFF0AF5  // 
-#define IR44_TURQUOISE     0xFF8A75  // 
-#define IR44_PURPLE        0xFFB24D  // 
-#define IR44_WARMWHITE     0xFF32CD  // warm white
+#define IR44_WARMWHITE2    0xFF12ED  // 
+#define IR44_ORANGE        0xFF0AF5  // DONE
+#define IR44_TURQUOISE     0xFF8A75  // DONE
+#define IR44_PURPLE        0xFFB24D  // DONE
+#define IR44_WARMWHITE     0xFF32CD  // 
 #define IR44_YELLOWISH     0xFF38C7  // 
-#define IR44_CYAN          0xFFB847  // 
-#define IR44_MAGENTA       0xFF7887  // 
-#define IR44_COLDWHITE     0xFFF807  // cold white
-#define IR44_YELLOW        0xFF18E7  // 
+#define IR44_CYAN          0xFFB847  // DONE
+#define IR44_MAGENTA       0xFF7887  // DONE
+#define IR44_COLDWHITE     0xFFF807  // 
+#define IR44_YELLOW        0xFF18E7  // DONE
 #define IR44_AQUA          0xFF9867  // 
-#define IR44_PINK          0xFF58A7  // 
-#define IR44_COLDWHITE2    0xFFD827  // coldest white
-#define IR44_REDPLUS       0xFF28D7  // 
-#define IR44_GREENPLUS     0xFFA857  // 
-#define IR44_BLUEPLUS      0xFF6897  // 
+#define IR44_PINK          0xFF58A7  // DONE
+#define IR44_COLDWHITE2    0xFFD827  // 
+#define IR44_REDPLUS       0xFF28D7  // DONE
+#define IR44_GREENPLUS     0xFFA857  // DONE
+#define IR44_BLUEPLUS      0xFF6897  // DONE
 #define IR44_QUICK         0xFFE817  // 
-#define IR44_REDMINUS      0xFF08F7  // 
-#define IR44_GREENMINUS    0xFF8877  // 
-#define IR44_BLUEMINUS     0xFF48B7  // 
+#define IR44_REDMINUS      0xFF08F7  // DONE
+#define IR44_GREENMINUS    0xFF8877  // DONE
+#define IR44_BLUEMINUS     0xFF48B7  // DONE
 #define IR44_SLOW          0xFFC837  // 
-#define IR44_DIY1          0xFF30CF  // 
-#define IR44_DIY2          0xFFB04F  // 
-#define IR44_DIY3          0xFF708F  // 
+#define IR44_DIY1          0xFF30CF  // 0
+#define IR44_DIY2          0xFFB04F  // 0
+#define IR44_DIY3          0xFF708F  // 0
 #define IR44_AUTO          0xFFF00F  // 
-#define IR44_DIY4          0xFF10EF  // 
-#define IR44_DIY5          0xFF906F  // 
-#define IR44_DIY6          0xFF50AF  // 
+#define IR44_DIY4          0xFF10EF  // 0
+#define IR44_DIY5          0xFF906F  // 0
+#define IR44_DIY6          0xFF50AF  // 0
 #define IR44_FLASH         0xFFD02F  // 
 #define IR44_JUMP3         0xFF20DF  // 
 #define IR44_JUMP7         0xFFA05F  // 
--- software/RGB LED controller/src/main.cpp
+++ software/RGB LED controller/src/main.cpp
@@ -11,9 +11,9 @@
 #define GMosfet D7
 #define BMosfet D5
 
-int RED = 0;
-int GREEN = 0;
-int BLUE = 0;
+int RED = 5;
+int GREEN = 5;
+int BLUE = 5;
 
 IRrecv irrecv(IRRecvPin);
 
@@ -32,10 +32,11 @@ void changeColor(int red, int green, int blue)
 
 /**
  * Saves a DIY setting to EEprom
+ * http://www.esp8266learning.com/read-and-write-to-the-eeprom-on-the-esp8266.php
+
  */
 void save(int diyNumber)
 {
-  //http://www.esp8266learning.com/read-and-write-to-the-eeprom-on-the-esp8266.php
   EEPROM.begin(512);
   // each one has 3 byte, one pur RGB value
   switch (diyNumber)
@@ -73,21 +74,76 @@ void save(int diyNumber)
   default:
     break;
   }
-  EEPROM.commit();
+  EEPROM.commit(); // store the data in eeprom
 }
 
-void
+/**
+ * loads a config from eprom
+ */
+void load(int diyNumber)
+{
+  EEPROM.begin(512);
+  // each one has 3 byte, one pur RGB value
+  switch (diyNumber)
+  {
+  case 1:
+    RED = EEPROM.read(0);
+    GREEN = EEPROM.write(1);
+    BLUE = EEPROM.write(2);
+    break;
+  case 2:
+    RED = EEPROM.read(3);
+    GREEN = EEPROM.write(4);
+    BLUE = EEPROM.write(5);
+    break;
+  case 3:
+    RED = EEPROM.read(6);
+    GREEN = EEPROM.write(7);
+    BLUE = EEPROM.write(8);
+    break;
+  case 4:
+    RED = EEPROM.read(9);
+    GREEN = EEPROM.write(10);
+    BLUE = EEPROM.write(11);
+    break;
+  case 5:
+    RED = EEPROM.read(12);
+    GREEN = EEPROM.write(13);
+    BLUE = EEPROM.write(14);
+    break;
+  case 6:
+    RED = EEPROM.read(15);
+    GREEN = EEPROM.write(16);
+    BLUE = EEPROM.write(17);
+    break;
+  default:
+    break;
+  }
+}
 
 /**
  * Execute the command send byh the remote
  */
 void executeRemoteCommand(int hexCommand)
 {
-  // Turn off
+  //==========
+  // CONTROL
+  //==========
+  //Turn on
+  if (hexCommand == IR44_OFF)
+  {
+    changeColor(10, 10, 10);
+  }
+
+  //Turn off
   if (hexCommand == IR44_OFF)
   {
     changeColor(0, 0, 0);
   }
+
+  //==========
+  // BRIGTNESS
+  //==========
   //Brigtness down
   else if (hexCommand == IR44_BMINUS)
   {
@@ -102,6 +158,79 @@ void executeRemoteCommand(int hexCommand)
     GREEN = GREEN + 5;
     BLUE = BLUE + 5;
   }
+
+  //==========
+  // DIY CONTROL
+  //==========
+  //plus red
+  else if (hexCommand == IR44_REDPLUS)
+  {
+    RED = RED + 1;
+  }
+  //plus green
+  else if (hexCommand == IR44_GREENPLUS)
+  {
+    GREEN = GREEN + 1;
+  }
+  //plus blue
+  else if (hexCommand == IR44_BLUEPLUS)
+  {
+    BLUE = BLUE + 1;
+  }
+
+  //minus red
+  else if (hexCommand == IR44_REDMINUS)
+  {
+    RED = RED - 1;
+  }
+  //minus green
+  else if (hexCommand == IR44_GREENMINUS)
+  {
+    GREEN = GREEN - 1;
+  }
+  //minus blue
+  else if (hexCommand == IR44_BLUEMINUS)
+  {
+    BLUE = BLUE - 1;
+  }
+
+  //==========
+  // DIY CONTROL SAVE
+  //==========
+  //DIY1
+  else if (hexCommand == IR44_DIY1)
+  {
+    load(1);
+  }
+  //DIY2
+  else if (hexCommand == IR44_DIY2)
+  {
+    load(2);
+  }
+  //DIY3
+  else if (hexCommand == IR44_DIY3)
+  {
+    load(3);
+  }
+  //DIY4
+  else if (hexCommand == IR44_DIY4)
+  {
+    load(4);
+  }
+  //DIY5
+  else if (hexCommand == IR44_DIY5)
+  {
+    load(5);
+  }
+  //DIY6
+  else if (hexCommand == IR44_DIY6)
+  {
+    load(6);
+  }
+
+  //==========
+  // INDIVIDUAL COLORS
+  //==========
   //Full red
   else if (hexCommand == IR44_RED)
   {
@@ -121,6 +250,36 @@ void executeRemoteCommand(int hexCommand)
   else if (hexCommand == IR44_WHITE)
   {
     changeColor(255, 255, 255);
+  }
+  //Cyan
+  else if (hexCommand == IR44_CYAN)
+  {
+    changeColor(0, 255, 255);
+  }
+  //Magenta
+  else if (hexCommand == IR44_MAGENTA)
+  {
+    changeColor(255, 0, 255);
+  }
+  //Orange
+  else if (hexCommand == IR44_ORANGE)
+  {
+    changeColor(255, 165, 0);
+  }
+  //Turqoise
+  else if (hexCommand == IR44_TURQUOISE)
+  {
+    changeColor(64, 224, 208);
+  }
+  //Yellow
+  else if (hexCommand == IR44_YELLOW)
+  {
+    changeColor(255, 255, 0);
+  }
+  //Pink
+  else if (hexCommand == IR44_PINK)
+  {
+    changeColor(255, 192, 203);
   }
 }
 
@@ -148,9 +307,16 @@ void loop()
     executeRemoteCommand(results.value); // execute the hex
     irrecv.resume();                     // Receive the next value
   }
+
   // output the colors
   analogWrite(RMosfet, RED);
   analogWrite(GMosfet, GREEN);
   analogWrite(BMosfet, BLUE);
+
+  Serial.println(RED);
+  Serial.println(GREEN);
+  Serial.println(BLUE);
+  Serial.println("----");
+
   delay(100);
 }
